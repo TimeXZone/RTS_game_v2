6d4277c14353c51d66466c9fe1e0035c1d56a43d
545157437 <545157437@qq.com>
2021-02-20 13:27:59 +0800

    2

---
+++ Source/RTS_game/GamePlay_Actor.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GamePlay_Actor.h"
+
+// Sets default values
+AGamePlay_Actor::AGamePlay_Actor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+bool AGamePlay_Actor::general_policy_func(bool active, int increase_rate_inc)
+{
+	if (active)
+	{
+		increase_rate += increase_rate_inc;
+		return true;
+	}
+	return false;
+}
+
+// Called when the game starts or when spawned
+void AGamePlay_Actor::BeginPlay()
+{
+	Super::BeginPlay();
+	policy_point = 100;
+	increase_rate = 60;
+	count = 0;	
+}
+
+// Called every frame
+void AGamePlay_Actor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	count++;
+	if (count%increase_rate)
+	{
+		policy_point++;
+		count = 0;
+	}
+
+}
+
---
+++ Source/RTS_game/GamePlay_Actor.h
@@ -0,0 +1,35 @@
+//该类已集成每tick运算.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GamePlay_Actor.generated.h"
+
+UCLASS()
+class RTS_GAME_API AGamePlay_Actor : public AActor
+{
+	GENERATED_BODY()
+		//UPROPERTY(EditAnywhere, BlueprintReadWrite,Category = status)
+		//UFUNCTION(BlueprintCallable, Category = MyFunc)
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = status)
+	int policy_point;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = status)
+	int increase_rate; //该值越小，policy_point增加速率越快
+private:
+	int count;
+public:	
+	// Sets default values for this actor's properties
+	AGamePlay_Actor();
+	UFUNCTION(BlueprintCallable, Category = Policy)
+	bool general_policy_func(bool active, int increase_rate_inc);//bool表示该函数启用，int表示政策对increase,rate造成的影响
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
